6f5acc385c1171103af187878b0f06f7739e155a
Israel AREMOH <israelaremoh@gmail.com>
2026-02-18 23:50:59 +0000

    fix SMTP transporter

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.use(compression());
 // Zoho SMTP transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.zoho.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.ZOHO_EMAIL,
     pass: process.env.ZOHO_APP_PASSWORD,
